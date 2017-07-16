fb4f887590cddc8d18c8d13bf46c13e3fea290e6
TheDeriver <federicofiroozi@hotmail.com>
2017-07-16 15:26:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -11,8 +11,18 @@ function openTab(event, tabName) {
         mainTab[i].className = mainTab[i].className.replace(" active", "");
     }
     document.getElementById(tabName).style.display = "block";
-    document.getElementById(tabName).style.opacity = 1;
+    document.getElementById(tabName).style.opacity = 0.999;
     event.currentTarget.className += " active";
  	event.currentTarget.style.cursor = "default";
 
 }
+
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-102650082-1', 'auto');
+  ga('send', 'pageview');
+
